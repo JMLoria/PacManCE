9df95a629ce9c6ab8bb8c7298c2a8af36fe7953a
jose <joloria@estudiantec.cr>
2023-05-09 22:35:54 -0600

    Set mobile app 2

--- src/AndroidController/app/src/main/java/com/example/androidcontroller/MainActivity.java
+++ src/AndroidController/app/src/main/java/com/example/androidcontroller/MainActivity.java
@@ -3,12 +3,56 @@ package com.example.androidcontroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtLevel;
+    private TextView txtPoints;
+    private TextView txtLives;
+
+    private int level;
+    private int points;
+    private int lives;
+
+    private TextView txtTest;
+
+    public String move;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        level = 1;
+        points = 250;
+        lives = 3;
+
+        txtLevel = findViewById(R.id.txtLevel);
+        txtPoints = findViewById(R.id.txtPoints);
+        txtLives = findViewById(R.id.txtLives);
+        txtTest = findViewById(R.id.txtTest);
+
+    }
+
+    public void moveUp(View view){
+        move = "W";
+        txtTest.setText(move);
+    }
+
+    public void moveDown(View view){
+        move = "S";
+        txtTest.setText(move);
+    }
+
+    public void moveLeft(View view){
+        move = "A";
+        txtTest.setText(move);
+    }
+
+    public void moveRight(View view){
+        move = "D";
+        txtTest.setText(move);
     }
 }
