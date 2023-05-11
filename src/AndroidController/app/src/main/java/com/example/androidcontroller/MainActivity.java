988ffefa983439d17b4a295954ec8bfb6393b944
jose <joloria@estudiantec.cr>
2023-05-10 22:36:07 -0600

    Mobile app, gyroscope function

--- src/AndroidController/app/src/main/java/com/example/androidcontroller/MainActivity.java
+++ src/AndroidController/app/src/main/java/com/example/androidcontroller/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.androidcontroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.hardware.*;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private TextView txtLevel, txtPoints, txtLives, txtTest;
 
@@ -24,6 +26,9 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SensorManager sensorManager = (SensorManager) getBaseContext().getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST);
 
         level = 1;
         points = 250;
@@ -59,5 +64,32 @@ public class MainActivity extends AppCompatActivity  {
         move = "D";
         txtTest.setText(move);
         move = "";
+    }
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float X = sensorEvent.values[0], Z = sensorEvent.values[2];
+        if(X > 15){
+            move = "S";
+            txtTest.setText(move);
+            move = "";
+        } else if(X < -15){
+            move = "W";
+            txtTest.setText(move);
+            move = "";
+        }
+        if(Z > 15){
+            move = "A";
+            txtTest.setText(move);
+            move = "";
+        } else if(Z < -15){
+            move = "D";
+            txtTest.setText(move);
+            move = "";
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
